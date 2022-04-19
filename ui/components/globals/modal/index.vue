e51a5630a1d49113478953e936178195e265c505
vivianeglz <viviane.gonzalez@outlook.fr>
2022-04-19 21:00:54 +0200

    :sparkles: (ui) : Add basic modal

---
+++ ui/components/globals/modal/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="modal">
+    <div class="modal__container" :class="className">
+      <div class="modal__header">
+        <h3 v-if="title">{{ title }}</h3>
+        <button class="modal__btn-close" @click.prevent="$emit('close')">
+          <icon-close />
+        </button>
+      </div>
+      <slot />
+      <div v-if="thereIsFooter" class="modal__footer">
+        <slot name="footer" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { toRef, computed } from "vue";
+import { IconClose } from "@components/globals/icons";
+
+export default {
+  name: "Modal",
+  components: {
+    IconClose
+  },
+  props: {
+    title: {
+      type: String,
+      default: "Title"
+    },
+    size: {
+      type: String,
+      default: "s",
+      validator(value) {
+        return ["s", "m", "l"].includes(value);
+      }
+    }
+  },
+  setup(props, context) {
+    const size = toRef(props, "size");
+    const thereIsFooter = computed(() => (context.slots.footer ? true : false));
+    const className = computed(() => `modal__container--${size.value}`);
+    return { className, thereIsFooter };
+  }
+};
+</script>
